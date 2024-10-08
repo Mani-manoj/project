61052aafe69650cf500a12580090c1a6ce19b4d4
jenani <rnjenani003@gmail.com>
2024-10-08 09:58:00 +0530

    fix:added delete unwanted modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddComponent } from './add/add.component';
-import { TvhomeComponent } from './tvhome/tvhome.component';
-import { HomepageComponent } from '../home/homepage/homepage.component';
 import { AuthGuard } from './guard1.guard';
+import { HeaderComponent } from './add/header/header.component';
 
 const routes: Routes = [
-  {
-    path:'',
-    component:AddComponent
-  },
-  {
-    path:'tv-home',
-    component:TvhomeComponent
-  },
-  {
-    path: 'homepage',
-    loadChildren: () => import('../home/home.module').then(m => m.HomeModule), 
-    canActivate: [AuthGuard]
-  },
 
   {
-    path: 'auth',
-    loadChildren: () => import('../auth/auth.module').then(m => m.AuthModule)
+    path:'',
+    component:HeaderComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,36 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { AddComponent } from './add/add.component';
-import { TvhomeComponent } from './tvhome/tvhome.component';
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
-import { CardComponent } from './card/card.component';
-import { RouterModule } from '@angular/router';
-// import { HomepageComponent } from '../home/homepage/homepage.component';
-// import { HomeModule } from '../home/home.module';
-// import { TwoComponent } from './two/two.component';
-
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddComponent,
-    TvhomeComponent,
-    HeaderComponent,
-    BodyComponent,
-    FooterComponent,
-    CardComponent,
-    // SigninComponent
-    // HomepageComponent
-    
-    
-  
-    // One1Component,
-  // TwoComponent
   ],
   imports: [
     BrowserModule,
