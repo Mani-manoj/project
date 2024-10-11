3901ec6b2fa269c5bc92c0eefbd137edf81fc2fe
JenaniRagavan <rnjenani003@gmail.com>
2024-10-11 19:08:41 +0530

    fix:added delete budget module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { AddComponent } from './add/add.component';
-import { TvhomeComponent } from './tvhome/tvhome.component';
-import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
-import { FooterComponent } from './footer/footer.component';
-import { CardComponent } from './card/card.component';
-import { RouterModule } from '@angular/router';
-import { BudgetComponent } from './budget/budget.component';
 import { FormsModule } from '@angular/forms';
-// import { HomepageComponent } from '../home/homepage/homepage.component';
-// import { HomeModule } from '../home/home.module';
-// import { TwoComponent } from './two/two.component';
-
-=======
-import { RouterModule } from '@angular/router'
->>>>>>> 329a48bb75a3b1f8eb7a822508db918eee32e7f0
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    AddComponent,
-    TvhomeComponent,
-    HeaderComponent,
-    BodyComponent,
-    FooterComponent,
-    CardComponent,
-    BudgetComponent,
-
-    // SigninComponent
-    // HomepageComponent
-    
-    
-  
-    // One1Component,
-  // TwoComponent
-=======
->>>>>>> 329a48bb75a3b1f8eb7a822508db918eee32e7f0
   ],
   imports: [
     BrowserModule,
